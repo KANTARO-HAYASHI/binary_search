1e102b6e2f5c6b8c4f927b0528b3b9c5774e2e6d
KANTARO-HAYASHI <kankan.football@icloud.com>
2020-07-08 13:37:59 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -5,35 +5,20 @@ int k;
 int A[100000];
 
 
-
-int p (int m ){
-    int f ,g;
-    f = 0;
-    g =0 ;
-    for(f =0; f < n ; f++){
-        g = g + (A[f]+m-1)/m;
-    }
-    return g<= k;
-}
-
-
 int main(){
-    int i, lb, ub;
+  int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-      lb = 0;
-      ub = 10000;
-      while (ub - lb >1){
-          int mid = ( lb + ub) /2;
-          if(p(mid)){
-              ub=mid;
-          }
-          else {
-              lb = mid;
-          }
+       lb =-1;
+       ub = n;
+      while (ub - lb > 1){
+          int mid = (lb + ub) /2;
+          if(A[mid] >= k) ub = mid;
+          else lb =mid;
       }
-  printf("%d \n",ub);
-  return 0;
+  
+    printf("%d \n", ub);
+    return 0;
 }
